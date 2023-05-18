2fa054dddf4425ae297f5036e6940ccf4692bdd6
Steve Mason <stevem@brandwatch.com>
2023-05-18 13:39:43 +0100

    Implement PartitionCountFetcher

--- src/main/java/com/brandwatch/kafka_pod_autoscaler/PartitionCountFetcher.java
+++ src/main/java/com/brandwatch/kafka_pod_autoscaler/PartitionCountFetcher.java
@@ -1,7 +1,29 @@
 package com.brandwatch.kafka_pod_autoscaler;
 
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PartitionCountFetcher {
-    public int countPartitions(String bootstrapServers, String topicName) {
-        return 0;
+    public int countPartitions(@NonNull String bootstrapServers, @NonNull String topic) {
+        logger.info("Requesting kafka metrics for bootstrapServers={} and topic={}", bootstrapServers, topic);
+
+        var properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        try (var adminApi = KafkaAdminClient.create(properties)) {
+            return adminApi.describeTopics(List.of(topic)).values().get(topic).get().partitions().size();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
     }
 }
