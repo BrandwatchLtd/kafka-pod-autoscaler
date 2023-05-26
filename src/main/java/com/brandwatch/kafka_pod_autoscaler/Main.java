0063071d63f8a11fd168b8be8ead45095e274a11
Steve Mason <stevem@brandwatch.com>
2023-05-26 22:41:02 +0100

    feat: add probes to deployment
    ..and default resources while we're here

--- src/main/java/com/brandwatch/kafka_pod_autoscaler/Main.java
+++ src/main/java/com/brandwatch/kafka_pod_autoscaler/Main.java
@@ -1,13 +1,21 @@
 package com.brandwatch.kafka_pod_autoscaler;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.config.LeaderElectionConfiguration;
 import lombok.extern.slf4j.Slf4j;
 
+import com.brandwatch.kafka_pod_autoscaler.handlers.LivenessHandler;
+import com.brandwatch.kafka_pod_autoscaler.handlers.StartupHandler;
+
 @Slf4j
 public class Main {
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
         logger.info("Starting operator");
 
         LeaderElectionConfiguration leaderElectionConfiguration = new LeaderElectionConfiguration("kafka-pod-autoscaler");
@@ -17,5 +25,11 @@ public class Main {
 
         operator.register(new KafkaPodAutoscalerReconciler(new PartitionCountFetcher()));
         operator.start();
+
+        var server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
+        server.createContext("/startup", new StartupHandler(operator));
+        server.createContext("/healthz", new LivenessHandler(operator));
+        server.setExecutor(null);
+        server.start();
     }
 }
---
+++ src/main/java/com/brandwatch/kafka_pod_autoscaler/handlers/StartupHandler.java
@@ -0,0 +1,36 @@
+package com.brandwatch.kafka_pod_autoscaler.handlers;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import io.javaoperatorsdk.operator.Operator;
+
+public class StartupHandler implements HttpHandler {
+    private final Operator operator;
+
+    public StartupHandler(Operator operator) {
+        this.operator = operator;
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        if (operator.getRuntimeInfo().isStarted()) {
+            sendMessage(httpExchange, 200, "started");
+        } else {
+            sendMessage(httpExchange, 400, "not started yet");
+        }
+    }
+
+    public static void sendMessage(HttpExchange httpExchange, int code, String message)
+            throws IOException {
+        try (var outputStream = httpExchange.getResponseBody()) {
+            var bytes = message.getBytes(StandardCharsets.UTF_8);
+            httpExchange.sendResponseHeaders(code, bytes.length);
+            outputStream.write(bytes);
+            outputStream.flush();
+        }
+    }
+}
